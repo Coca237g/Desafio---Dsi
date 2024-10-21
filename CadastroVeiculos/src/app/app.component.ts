1e423feec9227a881f0a7c3db6e06be2cccd61b4
Coca <giovanisantosdmg@gmail.com>
2024-10-21 01:26:06 -0300

    Teste-02

--- CadastroVeiculos/src/app/app.component.ts
+++ CadastroVeiculos/src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { VeiculoModel } from './model/Veiculos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CadastroVeiculos';
+
+  VeiculosForm: FormGroup = new FormGroup({});
+  VeiculosLista: VeiculoModel[] = [];
+  VeiculosObj: VeiculoModel = new VeiculoModel();
+
+  constructor() {
+    this.createForm();
+    this.loadVeiculosData();
+  }
+
+  createForm() { debugger
+    this.VeiculosForm = new FormGroup({
+      id: new FormControl(this.VeiculosObj.id),
+      marca: new FormControl(this.VeiculosObj.marca),
+      ano: new FormControl(this.VeiculosObj.ano),
+      created_at: new FormControl(this.VeiculosObj.created_at),
+      updated_at: new FormControl(this.VeiculosObj.updated_at),
+      financiado: new FormControl(this.VeiculosObj.financiado),
+      modelo: new FormControl(this.VeiculosObj.modelo),
+      placa: new FormControl(this.VeiculosObj.placa)
+    });
+  }
+
+  loadVeiculosData() { debugger
+    if (typeof window !== 'undefined' && localStorage) {  // Verifica se localStorage está disponível
+      const oldData = localStorage.getItem("EmpData");
+      if (oldData != null) {
+        const parseData = JSON.parse(oldData);
+        this.VeiculosLista = parseData;
+      }
+    }
+  }
+
+  onSave() { debugger
+    if (typeof window !== 'undefined' && localStorage) {  // Verifica se localStorage está disponível
+      const oldData = localStorage.getItem("EmpData");
+      if (oldData != null) {
+        const parseData = JSON.parse(oldData);
+        this.VeiculosForm.controls['id'].setValue(parseData.length + 1);
+        this.VeiculosLista.unshift(this.VeiculosForm.value);
+      } else {
+        this.VeiculosLista.unshift(this.VeiculosForm.value);
+      }
+      // Salvar os dados atualizados no localStorage
+      localStorage.setItem("EmpData", JSON.stringify(this.VeiculosLista));
+    }
+  }
 }
